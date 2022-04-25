954f086c7c1b016bc77868f9db09c24683cf603d
jrsamson <bsamson@pivotal.io>
2022-04-25 06:52:45 -0600

    test java

---
+++ java/maven/src/main/java/io/paketo/demo/DemoController.java
@@ -0,0 +1,17 @@
+package io.paketo.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class DemoController {
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello TBS";
+
+    }
+}
